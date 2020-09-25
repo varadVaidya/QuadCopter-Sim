8e2e672c659069afa90044db0502a76cf1ecc2fb
Varad <vaidyavarad2001@gmail.com>
2020-09-25 10:17:49 +0530

    shifted the control dynamics form quadcopter to dynamics

--- model/dynamics.py
+++ model/dynamics.py
@@ -1,1 +1,76 @@
 import numpy as np
+import model.QuadCopter
+class controlDynamics:
+    
+    '''
+        the control dynamics of 3D quadrotor can be be written as:
+        xddot     [ 0 ]         [ 0 ] 
+        yddot  =  [ 0 ]  +  R * [ 0 ] 
+        zddot     [-g ]         [u_1] 
+        
+        where, R is the rotation matrix that maps stuff from body frame to world frame.
+        
+        [p]      [c(theta)  0  -c(phi)s(theta)]     [ phidot   ]
+        [q]    = [    0     1    sin(phi)     ]  *  [ thetadot ]
+        [r]      [s(theta)  0  c(phi)c(theta) ]     [ psidot   ]
+        
+        where p,q,r is the body frame angular velocities.
+        
+            [pdot]     [u_2]    [p]     [p]
+        I * [qdot]  =  [u_3]  - [q] * I [q]
+            [rdot]     [u_4]    [r]     [r]
+            
+        u1,u2,u3,u4 are the control input to the system. 
+        
+        
+        assuming the hover position at all times
+        that means the theta and phi are at all times zero.
+        
+        thus pqr will be equal to the derivative of the roll pitch yaw.
+        
+            
+        
+        The current plan is to use the dynamics function to solve for all the variables and then store then in the current_state
+        class for future use.
+        
+        '''
+        
+    def __init__(self):
+            super().__init__()
+    
+    def attitude_solver(self,attitude,u):
+        
+        '''
+        attitude should contain 
+        roll pitch yaw, rolldot,pitchdot,yawdot
+        '''
+        
+        roll,pitch,yaw,rolldot,pitchdot,yawdot = attitude # Unpack the attitude vector
+        
+        u1,u2,u3,u4 = u      # unpack the input vector
+        
+        attitude_derivative = [rolldot,pitchdot,yawdot,
+                                u2/model.QuadCopter.I[0,0], u3/model.QuadCopter.I[1,1], u4/model.QuadCopter.I[2,2] ]
+        return attitude_derivative
+    
+    def position_solver(self,positon,u,attitude):
+        '''
+        position must contain
+        x , y, z , xdot,ydot,zdot
+        attitude must contain
+        phi,theta,psi
+        '''
+        
+        x,y,z,xdot,ydot,zdot = positon
+        u1,u2,u3,u4 = u      # unpack the input vector
+        phi,theta,psi = attitude
+        
+        position_derivative =[xdot,ydot,zdot,
+                            (u1/model.QuadCopter.mass) * ( np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi) ) ,
+                            (u1/model.QuadCopter.mass) * ( np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi) )
+                            (u1/model.QuadCopter.mass) * ( np.cos(phi) * np.cos(theta) - model.QuadCopter.gravity )
+                            ]
+        
+        return position_derivative
+
+    

656c1452811d2982f01013853bf132ef071a3492
Varad <vaidyavarad2001@gmail.com>
2020-09-25 15:13:18 +0530

    commented out certain code

--- model/QuadCopter.py
+++ model/QuadCopter.py
@@ -42,73 +42,81 @@ class QuadRotor:
             #set the class and name all variable required according to the dynamics
                 
     
-    def dynamics(self,state,u):
-        '''
-        the control dynamics of 3D quadrotor can be be written as:
-        xddot     [ 0 ]         [ 0 ] 
-        yddot  =  [ 0 ]  +  R * [ 0 ] 
-        zddot     [-g ]         [u_1] 
+    ##### DONOT TOUCH STUFF BELOW..#####
+    ##### DONOT TOUCH STUFF BELOW..#####
+    ##### DONOT TOUCH STUFF BELOW..#####
+    ##### DONOT TOUCH STUFF BELOW..#####
         
-        where, R is the rotation matrix that maps stuff from body frame to world frame.
+    
+    
+    
+    # # # # # # # def dynamics(self,state,u):
+    # # # # # # #     '''
+    # # # # # # #     the control dynamics of 3D quadrotor can be be written as:
+    # # # # # # #     xddot     [ 0 ]         [ 0 ] 
+    # # # # # # #     yddot  =  [ 0 ]  +  R * [ 0 ] 
+    # # # # # # #     zddot     [-g ]         [u_1] 
         
-        [p]      [c(theta)  0  -c(phi)s(theta)]     [ phidot   ]
-        [q]    = [    0     1    sin(phi)     ]  *  [ thetadot ]
-        [r]      [s(theta)  0  c(phi)c(theta) ]     [ psidot   ]
+    # # # # # # #     where, R is the rotation matrix that maps stuff from body frame to world frame.
         
-        where p,q,r is the body frame angular velocities.
+    # # # # # # #     [p]      [c(theta)  0  -c(phi)s(theta)]     [ phidot   ]
+    # # # # # # #     [q]    = [    0     1    sin(phi)     ]  *  [ thetadot ]
+    # # # # # # #     [r]      [s(theta)  0  c(phi)c(theta) ]     [ psidot   ]
         
-            [pdot]     [u_2]    [p]     [p]
-        I * [qdot]  =  [u_3]  - [q] * I [q]
-            [rdot]     [u_4]    [r]     [r]
+    # # # # # # #     where p,q,r is the body frame angular velocities.
+        
+    # # # # # # #         [pdot]     [u_2]    [p]     [p]
+    # # # # # # #     I * [qdot]  =  [u_3]  - [q] * I [q]
+    # # # # # # #         [rdot]     [u_4]    [r]     [r]
             
-        u1,u2,u3,u4 are the control input to the system. 
+    # # # # # # #     u1,u2,u3,u4 are the control input to the system. 
         
         
-        assuming the hover position at all times
-        that means the theta and phi are at all times zero.
+    # # # # # # #     assuming the hover position at all times
+    # # # # # # #     that means the theta and phi are at all times zero.
         
-        thus pqr will be equal to the derivative of the roll pitch yaw.
+    # # # # # # #     thus pqr will be equal to the derivative of the roll pitch yaw.
         
             
-        '''
+    # # # # # # #     '''
         
-        '''
-        The current plan is to use the dynamics function to solve for all the variables and then store then in the current_state
-        class for future use.
+    # # # # # # #     '''
+    # # # # # # #     The current plan is to use the dynamics function to solve for all the variables and then store then in the current_state
+    # # # # # # #     class for future use.
         
-        '''
-        def attitude_solver(self,attitude,u):
-            '''
-            attitude should contain 
-            roll pitch yaw, rolldot,pitchdot,yawdot
-            '''
+    # # # # # # #     '''
+    # # # # # # #     def attitude_solver(self,attitude,u):
+    # # # # # # #         '''
+    # # # # # # #         attitude should contain 
+    # # # # # # #         roll pitch yaw, rolldot,pitchdot,yawdot
+    # # # # # # #         '''
             
-            roll,pitch,yaw,rolldot,pitchdot,yawdot = attitude # Unpack the attitude vector
+    # # # # # # #         roll,pitch,yaw,rolldot,pitchdot,yawdot = attitude # Unpack the attitude vector
             
-            u1,u2,u3,u4 = u      # unpack the input vector
+    # # # # # # #         u1,u2,u3,u4 = u      # unpack the input vector
             
-            attitude_derivative = [rolldot,pitchdot,yawdot,
-                                   u2/self.I[0,0], u3/self.I[1,1], u4/self.I[2,2] ]
-            return attitude_derivative
+    # # # # # # #         attitude_derivative = [rolldot,pitchdot,yawdot,
+    # # # # # # #                                u2/self.I[0,0], u3/self.I[1,1], u4/self.I[2,2] ]
+    # # # # # # #         return attitude_derivative
         
-        def position_solver(self,positon,u,attitude):
-            '''
-            position must contain
-            x , y, z , xdot,ydot,zdot
-            attitude must contain
-            phi,theta,psi
-            '''
+    # # # # # # #     def position_solver(self,positon,u,attitude):
+    # # # # # # #         '''
+    # # # # # # #         position must contain
+    # # # # # # #         x , y, z , xdot,ydot,zdot
+    # # # # # # #         attitude must contain
+    # # # # # # #         phi,theta,psi
+    # # # # # # #         '''
             
-            x,y,z,xdot,ydot,zdot = positon
-            u1,u2,u3,u4 = u      # unpack the input vector
-            phi,theta,psi = attitude
+    # # # # # # #         x,y,z,xdot,ydot,zdot = positon
+    # # # # # # #         u1,u2,u3,u4 = u      # unpack the input vector
+    # # # # # # #         phi,theta,psi = attitude
             
-            position_derivative =[xdot,ydot,zdot,
-                                  (u1/self.mass) * ( np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi) ) ,
-                                  (u1/self.mass) * ( np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi) )
-                                  (u1/self.mass) * ( np.cos(phi) * np.cos(theta) - self.gravity )
-                                  ]
+    # # # # # # #         position_derivative =[xdot,ydot,zdot,
+    # # # # # # #                               (u1/self.mass) * ( np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi) ) ,
+    # # # # # # #                               (u1/self.mass) * ( np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi) )
+    # # # # # # #                               (u1/self.mass) * ( np.cos(phi) * np.cos(theta) - self.gravity )
+    # # # # # # #                               ]
             
-            return position_derivative
+    # # # # # # #         return position_derivative
 
         
